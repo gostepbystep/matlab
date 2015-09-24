abc101b9492e00f3ecc72318a54aca3d4a9dd760
gostepbystep <1933588939@qq.com>
2015-09-25 01:42:16 +0800

    delete

--- bbs/loadPicture.m
+++
@@ -1,6 +0,0 @@
-function [ out ] = loadPicture( path)
-    imdata = imread(path);
-%     out = rgb2gray(imdata);
-    out = imdata;
-end
-
--- pattern recognition/1/stpGFunc.m
+++
@@ -1,6 +0,0 @@
-function [ out ] = stpGFunc( x, meanValue, invCov, c)
-    % the desision function
-    d = x - meanValue;
-    out = -0.5 * d' * invCov * d + c;
-end
-
