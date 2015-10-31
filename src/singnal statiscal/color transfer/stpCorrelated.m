fb2b9009d07566f728b0b8f8680f89822d42dbbe
gostepbystep <1933588939@qq.com>
2015-10-31 15:59:52 +0800

    color transfer

---
+++ src/singnal statiscal/color transfer/stpCorrelated.m
@@ -0,0 +1,46 @@
+function [ osrgb, otrgb, outImg] = stpCorrelated(spath, tpath)
+%% 参考论文Color Transfer in Correlated Color Space
+
+    %% 读取并转换颜色空间
+    osrgb = imread(spath);
+    otrgb = imread(tpath);
+    
+    srgb = im2double(osrgb);
+    trgb = im2double(otrgb);
+
+    [sm, sn, ~] = size(srgb);
+    [tm, tn, ~] = size(trgb);
+
+    srgb = reshape(srgb, sm*sn, 3);
+    trgb = reshape(trgb, tm*tn, 3);
+    
+    means = mean(srgb);
+    meant = mean(trgb);
+    
+    covs = cov(srgb);
+    covt = cov(trgb);
+    
+    [sU, sA, sV] = svd(covs);
+    [tU, tA, tV] = svd(covt); 
+    
+    sT = eye(4); sT(1:3, 4) = means';
+    tT = eye(4); tT(1:3, 4) = -meant';
+    sR = eye(4); sR(1:3, 1:3) = sU;
+    tR = eye(4); tR(1:3, 1:3) = inv(tU);
+    
+%     sS = diag( [sA(1,1), sA(2,2), sA(3,3), 1] );
+    tS = diag( [1/sqrt(tA(1,1)), 1/sqrt(tA(2,2)), 1/sqrt(tA(3,3)), 1] );
+    sS = diag( [sqrt(sA(1,1)), sqrt(sA(2,2)), sqrt(sA(3,3)), 1] );
+%     tS = diag( [1/(tA(1,1)), 1/(tA(2,2)), 1/(tA(3,3)), 1] );
+    
+    
+    tI = [trgb'; ones(1, tm*tn)];
+    
+    coef = sT * sR  * sqrt( sS * tS) * tR * tT;
+    I =  coef * tI;
+    
+    image = reshape(I(1:3, :)', tm, tn, 3);
+
+    outImg = im2uint8(image);
+end
+
---
+++ src/singnal statiscal/color transfer/testReinhard.m
@@ -0,0 +1,26 @@
+clear;
+close all;
+clc;
+
+%% 测试颜色迁移的代码
+func = {'stpReinhard'; 'stpCorrelated'};
+strs = {'Reinhard算法'; 'RGB颜色相关性算法'};
+nMothods = length(func);
+
+for i = 2 : nMothods;
+    [ srgb1, trgb1, outImg1] = feval(func{i}, 'data\\1.jpg', 'data\\3.jpg');
+    [ srgb2, trgb2, outImg2] = feval(func{i}, 'data\\6.jpg', 'data\\3.jpg');
+    [ srgb3, trgb3, outImg3] = feval(func{i}, 'data\\11.jpg', 'data\\3.jpg');
+    
+    % 绘图
+    figure;  subplot(3, 3, 1); imshow(srgb1);
+    hold on; subplot(3, 3, 2); imshow(trgb1);
+    hold on; subplot(3, 3, 3); imshow(outImg1);
+    hold on; subplot(3, 3, 4); imshow(srgb2);
+    hold on; subplot(3, 3, 5); imshow(trgb2);
+    hold on; subplot(3, 3, 6); imshow(outImg2);
+    hold on; subplot(3, 3, 7); imshow(srgb3);
+    hold on; subplot(3, 3, 8); imshow(trgb3);
+    hold on; subplot(3, 3, 9); imshow(outImg3);
+    hold on; subtitle(3, strs{i});
+end
